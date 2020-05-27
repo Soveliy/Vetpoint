a8ac9cf8b010cbc13afd444702d7a8506e2a83ae
shini322 <33940422+shini322@users.noreply.github.com>
2020-05-27 14:51:56 +0300

    Index

--- app/js/main.js
+++ app/js/main.js
@@ -11,35 +11,52 @@ $(".nav-toggle").click(function() {
   $(".header__menu").slideToggle();
 });
 
-  // $('.events__container .swiper-container').each(function(){
-  //   var events_slider = new Swiper(this, {
-  //     slidesPerView: 'auto',
-  //   loopedSlides:40,
-  //   slidesPerGroup: 1,
-  //   spaceBetween: 10,
-  //   autoplay:true,
-  //   loop:true,
-  //   navigation: {
-  //       nextEl: $(this).parent().find('.swiper-button-next'),
-  //       prevEl: $(this).parent().find('.swiper-button-prev'),
-  //   },
+  $('.stock .swiper-container').each(function(){
+    var events_slider = new Swiper(this, {
+      slidesPerView: 'auto',
+    loopedSlides:40,
+    slidesPerView: 1,
+    spaceBetween: 10,
+    autoplay:true,
+    loop:true,
+    navigation: {
+        nextEl: $(this).parent().find('.swiper-button-next'),
+        prevEl: $(this).parent().find('.swiper-button-prev'),
+    },
     
-  //   breakpoints: {
-  //       640: {
-  //         slidesPerGroup: 2,
-  //         spaceBetween: 20,
-  //       },
-  //       900: {
-  //         slidesPerGroup: 3,
-  //         spaceBetween: 20,
-  //       },
-  //       1200: {
-  //         slidesPerGroup: 4,
-  //         spaceBetween: 20,
-  //       },
-  //     } 
-  // });
-  // });
+  });
+  });
+
+  $('.services .swiper-container').each(function(){
+    var events_slider = new Swiper(this, {
+      slidesPerView: 'auto',
+    loopedSlides:40,
+    slidesPerView: 6,
+    spaceBetween: 80,
+    autoplay:true,
+    loop:true,
+    navigation: {
+        nextEl: $(this).parent().find('.swiper-button-next'),
+        prevEl: $(this).parent().find('.swiper-button-prev'),
+    },
+    
+  });
+  });
+  $('.reviews .swiper-container').each(function(){
+    var events_slider = new Swiper(this, {
+      slidesPerView: 'auto',
+    loopedSlides:40,
+    slidesPerView: 1,
+    spaceBetween: 80,
+    autoplay:true,
+    loop:true,
+    navigation: {
+        nextEl: $(this).parent().find('.swiper-button-next'),
+        prevEl: $(this).parent().find('.swiper-button-prev'),
+    },
+    
+  });
+  });
 
   // $('.players__slider .swiper-container').each(function(){
   //   var events_slider = new Swiper(this, {
